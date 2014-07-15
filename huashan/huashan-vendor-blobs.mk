20ebedb7f05035ae436777dac4be7f8e703f1db2
Adam Farden <adam@farden.cz>
2014-07-15 20:02:21 +0200

    huashan: Update blobs from 12.1.A.1.205

--- huashan/huashan-vendor-blobs.mk
+++ huashan/huashan-vendor-blobs.mk
@@ -27,7 +27,6 @@ PRODUCT_COPY_FILES += \
     vendor/sony/huashan/proprietary/bin/illumination_service:system/bin/illumination_service \
     vendor/sony/huashan/proprietary/bin/irsc_util:system/bin/irsc_util \
     vendor/sony/huashan/proprietary/bin/ks:system/bin/ks \
-    vendor/sony/huashan/proprietary/bin/mm-pp-daemon:system/bin/mm-pp-daemon \
     vendor/sony/huashan/proprietary/bin/mpdecision:system/bin/mpdecision \
     vendor/sony/huashan/proprietary/bin/netmgrd:system/bin/netmgrd \
     vendor/sony/huashan/proprietary/bin/qcks:system/bin/qcks \
